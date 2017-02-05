0b0498d0dbe90c5677f976247f6d43b3d6ea5605
Praveen C <cpraveen@gmail.com>
2017-02-05 22:39:05 +0530

    Minor updates

--- fenics/step-06/demo.py
+++ fenics/step-06/demo.py
@@ -15,8 +15,11 @@ def BottomRight(x, on_boundary):
 def Boundary(x, on_boundary):
    return x[1] > DOLFIN_EPS and on_boundary
 
+# Polynomial degree
+degree = 1
+
 # Exact solution
-ue = Expression('(1.0/pi)*atan2(x[1], x[0])')
+ue = Expression('(1.0/pi)*atan2(x[1], x[0])',degree=degree+3)
 
 mesh = Mesh('mesh.xml')
 
@@ -24,7 +27,7 @@ nstep= 5
 conv = []
 file = File('sol.pvd')
 for j in range(nstep):
-   V = FunctionSpace(mesh, 'CG', 1)
+   V = FunctionSpace(mesh, 'CG', degree)
 
    u = TrialFunction(V)
    v = TestFunction(V)
@@ -50,19 +53,20 @@ for j in range(nstep):
    w.rename("sol","sol")
    file << w
    error_L2 = errornorm(ue, w, norm_type='L2', degree_rise=3)
-   conv.append([mesh.hmax(), error_L2])
+   error_H1 = errornorm(ue, w, norm_type='H10', degree_rise=3)
+   conv.append([mesh.hmax(), error_L2, error_H1])
 
    # refine the mesh
    mesh = refine(mesh)
 
 # Compute convergence rate
-print "---------------------------------------"
+print "----------------------------------------------------------------------"
+fmt='{0:14.6e} {1:14.6e} {2:10.3f} {3:14.6e} {4:10.3f}'
 for j in range(nstep):
    if j==0:
-      fmt='{0:14.6e} {1:14.6e}'
-      print fmt.format(conv[j][0], conv[j][1])
+      print fmt.format(conv[j][0], conv[j][1], 0, conv[j][2], 0)
    else:
       rate_L2 = log(conv[j-1][1]/conv[j][1])/log(2)
-      fmt='{0:14.6e} {1:14.6e} {2:10.3f}'
-      print fmt.format(conv[j][0], conv[j][1], rate_L2)
-print "---------------------------------------"
+      rate_H1 = log(conv[j-1][2]/conv[j][2])/log(2)
+      print fmt.format(conv[j][0], conv[j][1], rate_L2, conv[j][2], rate_H1)
+print "----------------------------------------------------------------------"
