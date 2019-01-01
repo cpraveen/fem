5d617a87e37a02becd44d56272c3880c97a2b41f
Praveen C <cpraveen@gmail.com>
2019-01-01 08:58:45 +0530

    Added ssprk43 scheme in fourier analysis

--- dg1d/scalar/fourier.py
+++ dg1d/scalar/fourier.py
@@ -9,7 +9,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-degree', type=int, help='Degree', required=True)
 parser.add_argument('-cfl_min', type=float, help='Min cfl', default=0.0)
 parser.add_argument('-cfl_max', type=float, help='Max cfl', default=1.0)
-parser.add_argument('-scheme', choices=('ssprk22','ssprk33'),
+parser.add_argument('-scheme', choices=('ssprk22','ssprk33','ssprk43'),
                     help='Time scheme', required=True)
 args = parser.parse_args()
 
@@ -31,11 +31,16 @@ for i in range(Nq):
 # Identity
 I   = np.eye(nd)
 
-def amplification_matrix(scheme, G):
+def amplification_matrix(scheme, nu, C):
     if scheme == 'ssprk22':
+        G = I + nu*C
         H = 0.5*(I + G@G)
     elif scheme == 'ssprk33':
+        G = I + nu*C
         H = (1.0/3.0)*I + (1.0/2.0)*G + (1.0/6.0)*(G@G@G)
+    elif scheme == 'ssprk43':
+        G = I + 0.5*nu*C
+        H = (2.0/3.0)*G + (1.0/3.0)*G@G@G@G
     else:
         print('Unknown time scheme')
         exit()
@@ -62,8 +67,7 @@ for nu in cfls:
     maxeig = 0.0
     for kdx in wavenums:
         C = A + np.exp(-1j*kdx)*Bm - Bp
-        G = I + nu*C
-        H = amplification_matrix(args.scheme, G)
+        H = amplification_matrix(args.scheme, nu, C)
         eig = np.abs(eigvals(H)).max()
         if eig > maxeig:
             maxeig = eig
