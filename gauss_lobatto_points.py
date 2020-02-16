1ee215f00273fc2dfb53ac2198fdbc2d19d9d416
Praveen C <cpraveen@gmail.com>
2020-02-16 17:25:51 +0530

    Plot lagrange polynomials

--- gauss_lobatto_points.py
+++ gauss_lobatto_points.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.interpolate import lagrange
 
 # See section on "Gauss-Lobattto rules" here
 #    https://en.wikipedia.org/wiki/Gaussian_quadrature
@@ -32,7 +33,22 @@ def gauss_lobatto_points(n):
 
 # Plot the points
 for n in range(2,8):
-    plt.plot(gauss_lobatto_points(n), (10-n)*np.ones(n), 'o-')
+    x = 0.5*(1 + gauss_lobatto_points(n))
+    plt.plot(x, (10-n)*np.ones(n), 'o-')
 plt.yticks([])
 plt.title('Gauss-Lobatto points')
+
+for n in range(2,8):
+    plt.figure()
+    xs = 0.5*(1 + gauss_lobatto_points(n))
+    for i in range(n):
+        values = np.zeros((n,1))
+        values[i] = 1.0
+        fun = lagrange(xs, values)
+        x = np.linspace(0,1,100)
+        plt.plot(x, fun(x))
+        plt.xlabel('x')
+        plt.grid(True)
+        plt.title('Number of points = '+str(n))
+
 plt.show()
