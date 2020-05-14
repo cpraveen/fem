3c37211dd6803193d14fdfb68b288fca64a0b57f
Praveen C <cpraveen@gmail.com>
2020-05-14 11:56:14 +0530

    Improve the plotting code

---
+++ fenics/step-10/README.md
@@ -0,0 +1,13 @@
+# Poisson with discontinuous coefficient
+
+Run the code by setting ```refine_type = 'uniform'``` and  ```refine_type = 'adaptive'``` and visualize the meshes.
+
+On a general mesh, the discontinuous coefficient cannot be represented properly. There is a mesh created in Gmsh which can resolve the coefficient better. We can visualize it as follows
+```
+gmsh -2 -format msh2 mesh.geo
+dolfin-convert -i gmsh -o xml mesh.geo mesh.xml
+python3 plot_mu.pdf
+open mu1.pdf mu2.pdf
+```
+
+**Exercise:** Use the Gmsh mesh and run the adaptive refinement.
--- fenics/step-10/plot_mu.py
+++ fenics/step-10/plot_mu.py
@@ -1,24 +1,34 @@
+'''
+Visualize the discontinuous coefficient in the Poisson equation
+
+Before running this code, generate mesh like this:
+    gmsh -2 -format msh2 mesh.geo
+    dolfin-convert -i gmsh -o xml mesh.msh mesh.xml
+'''
 from dolfin import *
 import matplotlib.pyplot as plt
 
 mu1 = 1.0
 mu2 = 10.0
+mu = Expression('x[0]*x[0] + x[1]*x[1] <= 0.5*0.5 ? mu1 : mu2',
+                mu1=mu1, mu2=mu2, degree=0)
 
-class Coefficient(UserExpression):
-   def eval(self, values, x):
-      if x[0]**2 + x[1]**2 <= 0.5**2:
-         values[0] = mu1
-      else:
-         values[0] = mu2
-   def value_shape(self):
-         return ()
+def plot_mu(mesh,fname):
+    V0 = FunctionSpace(mesh,'DG',0)
+    mu0 = project(mu,V0)
+
+    plt.figure(figsize=(12,6))
+    plt.subplot(121)
+    plot(mesh)
+    plt.subplot(122)
+    c = plot(mu0)
+    plt.colorbar(c)
+    plt.savefig(fname)
+    print('Coefficient mu printed to '+fname)
 
 n = 10
 mesh = RectangleMesh(Point(-1.0, -1.0), Point(+1.0, +1.0), n, n)
-mu = Coefficient(degree=0)
-V0 = FunctionSpace(mesh,'DG',0)
-mu = project(mu,V0)
-c = plot(mu)
-plt.colorbar(c)
-plt.savefig('mu.pdf')
-print('Coefficient mu printed to mu.pdf')
+plot_mu(mesh,'mu1.pdf')
+
+mesh = Mesh('mesh.xml')
+plot_mu(mesh,'mu2.pdf')
