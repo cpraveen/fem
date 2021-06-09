ac9a6107d0074e1cb3362830cfc7266ca6ea2ac2
Praveen C <cpraveen@gmail.com>
2021-06-09 12:27:50 +0530

    Example of tri grid in python

---
+++ python/2d/mesh_tri.py
@@ -0,0 +1,42 @@
+import meshio
+import numpy as np
+import matplotlib.pyplot as plt
+
+mesh = meshio.read("mesh_tri.msh")
+
+x, y = mesh.points[:,0], mesh.points[:,1]
+tri = mesh.cells_dict["triangle"]
+faces = mesh.cells_dict["line"]
+
+# Show mesh
+plt.figure()
+plt.triplot(x, y, tri)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Mesh')
+plt.axis('equal')
+
+# Show boundary faces
+plt.figure()
+for face in faces:
+    xf = [ x[face[0]], x[face[1]] ]
+    yf = [ y[face[0]], y[face[1]] ]
+    plt.plot(xf, yf)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Boundary faces')
+plt.axis('equal')
+
+# Some smooth function of (x,y)
+u = np.sin(2.0*np.pi*x) * np.sin(2.0*np.pi*y)
+
+plt.figure()
+c = plt.tricontourf(x, y, tri, u, cmap='rainbow', levels=20)
+plt.colorbar(c)
+plt.tricontour(x, y, tri, u, colors='k', levels=20)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Solution')
+plt.axis('equal')
+
+plt.show()
