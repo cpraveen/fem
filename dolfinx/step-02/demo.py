724852f8b760c527e84a74089f1ad3a2ffe7beff
Praveen C <cpraveen@gmail.com>
2022-10-21 08:31:24 +0530

    Add dolfinx version of step-02

--- dolfinx/step-02/demo.py
+++ dolfinx/step-02/demo.py
@@ -1,15 +1,18 @@
 """
--Laplace(u) = 1 in (0,1)x(0,1)
-         u  = 0 on boundary
+-Laplace(u) = f in (0,1)x(0,1)
+         u  = g on boundary
+Choose exact solution as u(x,y) = sin(2*pi*x) cos(2*pi*y) so that the rhs
+is given by f(x,y) = (2*pi)^2 u(x,y). The BC g is obtained from exact solution.
 """
 from mpi4py import MPI
 from dolfinx import mesh
 from dolfinx.fem import FunctionSpace,Function,Constant,dirichletbc
 from dolfinx.fem import locate_dofs_topological
 from dolfinx.fem.petsc import LinearProblem
-from ufl import TrialFunction,TestFunction,dx,inner,grad
+from ufl import TrialFunction,TestFunction,dx,inner,grad,SpatialCoordinate
 from petsc4py.PETSc import ScalarType
 from dolfinx.io import VTKFile,XDMFFile
+from numpy import pi,sin,cos
 
 domain = mesh.create_unit_square(MPI.COMM_WORLD, 20, 20, 
                                  mesh.CellType.triangle)
@@ -22,7 +25,8 @@ v = TestFunction(V)
 a = inner(grad(u), grad(v))*dx
 
 # Linear functional
-f = Constant(domain, ScalarType(1))
+f = Function(V)
+f.interpolate(lambda x: 8*pi*pi*sin(2*pi*x[0])*cos(2*pi*x[1]))
 L = f*v*dx
 
 # Create facet to cell connectivity required to determine boundary facets
@@ -32,9 +36,10 @@ domain.topology.create_connectivity(fdim, tdim)
 boundary_facets = mesh.exterior_facet_indices(domain.topology)
 
 # Dirichlet bc
-g = Constant(domain, ScalarType(0))
+g = Function(V)
+g.interpolate(lambda x: sin(2*pi*x[0])*cos(2*pi*x[1]))
 boundary_dofs = locate_dofs_topological(V, fdim, boundary_facets)
-bc = dirichletbc(g, boundary_dofs, V)
+bc = dirichletbc(g, boundary_dofs)
 
 # Define linear problem and solve
 problem = LinearProblem(a, L, bcs=[bc], 
