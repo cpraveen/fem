89df34bbcecd34b4d538db28a9e2fc6cb8b249be
Praveen C <cpraveen@gmail.com>
2024-09-13 14:06:43 +0530

    Run astyle

--- dg1d/deal.II/1d_system_legendre/shallow/pde.h
+++ dg1d/deal.II/1d_system_legendre/shallow/pde.h
@@ -1,7 +1,5 @@
 //------------------------------------------------------------------------------
-// PDE is linear acoustics
-//    p_t + K * v_x = 0
-//    v_t + (1/rho) * p_x = 0
+// 1d shallow water equation with flat bottom
 //------------------------------------------------------------------------------
 
 using namespace dealii;
@@ -13,7 +11,7 @@ const unsigned int nvar = 2;
 // Extend namespace Problem with data specific to this pde
 // This data must be set in problem_data.h file.
 //------------------------------------------------------------------------------
-namespace Problem 
+namespace Problem
 {
    extern double g;
 }
@@ -24,112 +22,115 @@ namespace Problem
 namespace PDE
 {
 
-const double g = Problem::g;
+   const double g = Problem::g;
 
 // Numerical flux functions
-enum class FluxType {roe, rusanov};
+   enum class FluxType {roe, rusanov};
 
-std::map<std::string, FluxType> FluxTypeList{{"roe",     FluxType::roe}, 
-                                             {"rusanov", FluxType::rusanov}};
+   std::map<std::string, FluxType> FluxTypeList{{"roe",     FluxType::roe},
+                                                {"rusanov", FluxType::rusanov}};
 
 //------------------------------------------------------------------------------
 // Flux of the PDE model: f(u)
 //------------------------------------------------------------------------------
-void
-physical_flux(const Vector<double>& u, 
-              const Point<1>&       /*p*/, 
-              Vector<double>&       flux)
-{
-   const double h = u[0];
-   const double v = u[1] / h;
-   flux[0] = h * v;
-   flux[1] = h * pow(v,2) + 0.5 * g * pow(h,2);
-}
+   void
+   physical_flux(const Vector<double>& u,
+                 const Point<1>&       /*p*/,
+                 Vector<double>&       flux)
+   {
+      const double h = u[0];
+      const double v = u[1] / h;
+      flux[0] = h * v;
+      flux[1] = h * pow(v, 2) + 0.5 * g * pow(h, 2);
+   }
 
 //------------------------------------------------------------------------------
 // Maximum wave speed: |df/du(u)|
 //------------------------------------------------------------------------------
-double
-max_speed(const Vector<double>& u, 
-          const Point<1>&       /*p*/)
-{
-   const double h = u[0];
-   const double v = u[1] / h;
-   return abs(v) + sqrt(g * h);
-}
+   double
+   max_speed(const Vector<double>& u,
+             const Point<1>&       /*p*/)
+   {
+      const double h = u[0];
+      const double v = u[1] / h;
+      return abs(v) + sqrt(g* h);
+   }
 
 //------------------------------------------------------------------------------
 // R = matrix of right eigenvectors, columns are right eigenvectors
 // L = matrix of left eigenvectors = R^(-1), rows are left eigenvectors
 //------------------------------------------------------------------------------
-void 
-char_mat(const Vector<double>& /*u*/,
-         const Point<1>&       /*p*/,
-         FullMatrix<double>&   R,
-         FullMatrix<double>&   L)
-{
-   R(0,0) = 1.0; R(0,1) = 0.0;
-   R(1,0) = 0.0; R(1,1) = 1.0;
-
-   L(0,0) = 1.0; L(0,1) = 0.0;
-   L(1,0) = 0.0; L(1,1) = 1.0;
-}
-
-//------------------------------------------------------------------------------
-// Compute flux across cell faces
-//------------------------------------------------------------------------------
-void roe_flux(const Vector<double> &/*ul*/,
-              const Vector<double> &/*ur*/,
-              const Point<1>&       /*p*/,
-              Vector<double> &/*flux*/)
-{
-   AssertThrow(false, ExcNotImplemented());
-}
-
-//------------------------------------------------------------------------------
-// Compute flux across cell faces
-//------------------------------------------------------------------------------
-void rusanov_flux(const Vector<double> &ul,
-                  const Vector<double> &ur,
-                  const Point<1>&       p,
-                  Vector<double> &flux)
-{
-   Vector<double> fl(nvar);
-   physical_flux(ul, p, fl);
-
-   Vector<double> fr(nvar);
-   physical_flux(ur, p, fr);
-
-   double cl = max_speed(ul, p);
-   double cr = max_speed(ur, p);
-   double lam = std::max(cl, cr);
-
-   flux[0] = 0.5*(fl[0] + fr[0]) - 0.5 * lam * (ur[0] - ul[0]);
-   flux[1] = 0.5*(fl[1] + fr[1]) - 0.5 * lam * (ur[1] - ul[1]);
-}
-
-//------------------------------------------------------------------------------
-// Compute flux across cell faces
-//------------------------------------------------------------------------------
-void numerical_flux(const FluxType        flux_type,
-                    const Vector<double>& ul,
-                    const Vector<double>& ur,
-                    const Point<1>&       p,
-                    Vector<double>&       flux)
-{
-   switch (flux_type)
+   void
+   char_mat(const Vector<double>& /*u*/,
+            const Point<1>&       /*p*/,
+            FullMatrix<double>&   R,
+            FullMatrix<double>&   L)
    {
-   case FluxType::roe:
-      roe_flux(ul, ur, p, flux);
-      break;
+      R(0, 0) = 1.0; R(0, 1) = 0.0;
+      R(1, 0) = 0.0; R(1, 1) = 1.0;
 
-   case FluxType::rusanov:
-      rusanov_flux(ul, ur, p, flux);
-      break;
-
-   default:
-      AssertThrow(false, ExcMessage("Unknown flux type"));
+      L(0, 0) = 1.0; L(0, 1) = 0.0;
+      L(1, 0) = 0.0; L(1, 1) = 1.0;
    }
-}
+
+//------------------------------------------------------------------------------
+// Compute flux across cell faces
+//------------------------------------------------------------------------------
+   void
+   roe_flux(const Vector<double>& /*ul*/,
+            const Vector<double>& /*ur*/,
+            const Point<1>&       /*p*/,
+            Vector<double>&       /*flux*/)
+   {
+      AssertThrow(false, ExcNotImplemented());
+   }
+
+//------------------------------------------------------------------------------
+// Compute flux across cell faces
+//------------------------------------------------------------------------------
+   void
+   rusanov_flux(const Vector<double>& ul,
+                const Vector<double>& ur,
+                const Point<1>&       p,
+                Vector<double>&       flux)
+   {
+      Vector<double> fl(nvar);
+      physical_flux(ul, p, fl);
+
+      Vector<double> fr(nvar);
+      physical_flux(ur, p, fr);
+
+      double cl = max_speed(ul, p);
+      double cr = max_speed(ur, p);
+      double lam = std::max(cl, cr);
+
+      flux[0] = 0.5 * (fl[0] + fr[0]) - 0.5 * lam * (ur[0] - ul[0]);
+      flux[1] = 0.5 * (fl[1] + fr[1]) - 0.5 * lam * (ur[1] - ul[1]);
+   }
+
+//------------------------------------------------------------------------------
+// Compute flux across cell faces
+//------------------------------------------------------------------------------
+   void
+   numerical_flux(const FluxType        flux_type,
+                  const Vector<double>& ul,
+                  const Vector<double>& ur,
+                  const Point<1>&       p,
+                  Vector<double>&       flux)
+   {
+      switch(flux_type)
+      {
+         case FluxType::roe:
+            roe_flux(ul, ur, p, flux);
+            break;
+
+         case FluxType::rusanov:
+            rusanov_flux(ul, ur, p, flux);
+            break;
+
+         default:
+            AssertThrow(false, ExcMessage("Unknown flux type"));
+      }
+   }
 
 }
--- dg1d/deal.II/1d_system_legendre/shallow/problem_data.h
+++ dg1d/deal.II/1d_system_legendre/shallow/problem_data.h
@@ -3,44 +3,45 @@ using namespace dealii;
 namespace Problem
 {
 
-double xmin = -5.0;
-double xmax =  5.0;
-double final_time = 2.0;
-bool periodic = false;
+   double xmin = -5.0;
+   double xmax =  5.0;
+   double final_time = 2.0;
+   bool periodic = false;
 
 // PDE data
-double g = 1.0;
+   double g = 1.0;
 
 //------------------------------------------------------------------------------
 // Initial condition
 //------------------------------------------------------------------------------
-void
-initial_value(const Point<1>& p,
-              Vector<double>&   values)
-{
-   double x = p[0];
-   if(x < 0.5)
+   void
+   initial_value(const Point<1>& p,
+                 Vector<double>& values)
    {
-      values[0] = 3.0;
-      values[1] = 0.0;
+      double x = p[0];
+      if(x < 0.0)
+      {
+         values[0] = 3.0;
+         values[1] = 0.0;
+      }
+      else
+      {
+         values[0] = 1.0;
+         values[1] = 0.0;
+      }
    }
-   else
-   {
-      values[0] = 1.0;
-      values[1] = 0.0;
-   }
-}
 
 //------------------------------------------------------------------------------
 // Boundary condition
 //------------------------------------------------------------------------------
-void boundary_value(const int /*id*/,
-                    const double /*t*/,
-                    const Vector<double> & ul,
-                    Vector<double> & ur)
-{
-   // Neumann bc, we assume waves do not reach boundary
-   ur = ul;
-}
+   void
+   boundary_value(const int             /*id*/,
+                  const double          /*t*/,
+                  const Vector<double>& ul,
+                  Vector<double>&       ur)
+   {
+      // Neumann bc, we assume waves do not reach boundary
+      ur = ul;
+   }
 
 }
