538b5384b5bf5d872eb9b37bfbc5ec8f1b391b98
Praveen C <cpraveen@gmail.com>
2024-09-16 09:03:56 +0530

    Shorten postprocessor code

--- dg2d/system_legendre/euler/pde.h
+++ dg2d/system_legendre/euler/pde.h
@@ -357,59 +357,28 @@ namespace PDE
    public:
       void
       evaluate_vector_field(const DataPostprocessorInputs::Vector<dim> &input_data,
-                            std::vector<Vector<double>> &computed_quantities) const override;
-      std::vector<std::string> get_names() const override;
-      UpdateFlags get_needed_update_flags() const override;
+                            std::vector<Vector<double>> &computed_quantities) const override
+      {
+         const std::vector<Vector<double>> &uh = input_data.solution_values;
+         Assert(uh[0].size() == nvar && computed_quantities[0].size() == nvar,
+                ExcInternalError());
+         for (unsigned int q = 0; q < uh.size(); ++q)
+         {
+            con2prim<dim>(uh[q], computed_quantities[q]);
+         }
+      }
+      std::vector<std::string> get_names() const override
+      {
+         if (dim == 2)
+            return {"Density", "XVelocity", "YVelocity", "Pressure"};
+         else
+            return {"Density", "XVelocity", "YVelocity", "ZVelocity", "Pressure"};
+      }
+      UpdateFlags get_needed_update_flags() const override
+      {
+         return update_values;
+      }
    };
 
-   //---------------------------------------------------------------------------
-   template <int dim>
-   void
-   Postprocessor<dim>::evaluate_vector_field(const DataPostprocessorInputs::Vector<dim> &input_data,
-                                             std::vector<Vector<double>> &computed_quantities) const
-   {
-      const std::vector<Vector<double>> &uh = input_data.solution_values;
-      const unsigned int n_quadrature_points = uh.size();
-
-      Assert(computed_quantities.size() == n_quadrature_points,
-             ExcInternalError());
-
-      Assert(uh[0].size() == nvar,
-             ExcInternalError());
-
-      Assert(computed_quantities[0].size() == nvar,
-             ExcInternalError());
-
-      for (unsigned int q = 0; q < n_quadrature_points; ++q)
-      {
-         con2prim<dim>(uh[q], computed_quantities[q]);
-      }
-   }
-
-   //---------------------------------------------------------------------------
-   template <int dim>
-   std::vector<std::string>
-   Postprocessor<dim>::get_names() const
-   {
-      if (dim == 2)
-         return { "Density",
-                  "XVelocity",
-                  "YVelocity",
-                  "Pressure" };
-      else
-         return { "Density",
-                  "XVelocity",
-                  "YVelocity",
-                  "ZVelocity",
-                  "Pressure"};
-   }
-
-   //---------------------------------------------------------------------------
-   template <int dim>
-   UpdateFlags
-   Postprocessor<dim>::get_needed_update_flags() const
-   {
-      return update_values;
-   }
 }
 #endif
