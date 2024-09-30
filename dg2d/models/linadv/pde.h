8317aa119b32faaf8533fcb7fdc30a4cfca50e59
Praveen C <cpraveen@gmail.com>
2024-09-30 10:40:36 +0530

    Add linear adv model

---
+++ dg2d/models/linadv/pde.h
@@ -0,0 +1,160 @@
+//------------------------------------------------------------------------------
+// Euler equations for compressible flows
+//------------------------------------------------------------------------------
+#ifndef __PDE_H__
+#define __PDE_H__
+
+#include <deal.II/numerics/data_postprocessor.h>
+
+using namespace dealii;
+
+const unsigned int nvar = 1;
+
+// Numerical flux functions
+enum class FluxType {upwind, none};
+
+std::map<std::string, FluxType> FluxTypeList{{"upwind", FluxType::upwind},
+                                             {"none",   FluxType::none}};
+
+//------------------------------------------------------------------------------
+// This should be set by user in a problem.h file
+//------------------------------------------------------------------------------
+namespace ProblemData
+{
+   template <int dim>
+   extern void velocity(const Point<dim>& p, Tensor<1,dim>& v);
+}
+
+//------------------------------------------------------------------------------
+namespace PDE
+{
+
+   const std::string name = "2D linear advection equation";
+   using ProblemData::velocity;
+
+   //---------------------------------------------------------------------------
+   template <int dim>
+   void
+   upwind_flux(const Vector<double>&  ul,
+               const Vector<double>&  ur,
+               const Point<dim>&      p,
+               const Tensor<1, dim>&  normal,
+               Vector<double>&        flux)
+   {
+      Tensor<1,dim> vel;
+      velocity(p, vel);
+      const auto vn = vel * normal;
+      flux[0] = vn * ((vn > 0.0) ? ul[0] : ur[0]);
+   }
+
+   //---------------------------------------------------------------------------
+   // Following functions are directly called from DG solver
+   //---------------------------------------------------------------------------
+   template <int dim>
+   void
+   max_speed(const Vector<double>& /*u*/,
+             const Point<dim>&     p,
+             Tensor<1, dim>&       speed)
+   {
+      velocity(p, speed);
+   }
+
+   //---------------------------------------------------------------------------
+   // Flux of the PDE model: f(u,x)
+   //---------------------------------------------------------------------------
+   template <int dim>
+   void
+   physical_flux(const Vector<double>&       u,
+                 const Point<dim>&           p,
+                 ndarray<double, nvar, dim>& flux)
+   {
+      Tensor<1,dim> vel;
+      velocity(p, vel);
+      flux[0][0] = vel[0] * u[0];
+      flux[0][1] = vel[1] * u[0];
+   }
+
+   //---------------------------------------------------------------------------
+   // Compute flux across cell faces
+   //---------------------------------------------------------------------------
+   template <int dim>
+   void
+   numerical_flux(const FluxType        flux_type,
+                  const Vector<double>& ul,
+                  const Vector<double>& ur,
+                  const Point<dim>&     p,
+                  const Tensor<1, dim>& normal,
+                  Vector<double>&       flux)
+   {
+      switch(flux_type)
+      {
+         case FluxType::upwind:
+            upwind_flux(ul, ur, p, normal, flux);
+            break;
+
+         default:
+            AssertThrow(false, ExcMessage("Unknown numerical flux"));
+      }
+   }
+
+   //---------------------------------------------------------------------------
+   template <int dim>
+   void
+   boundary_flux(const Vector<double>& ul,
+                 const Vector<double>& ur,
+                 const Point<dim>&     p,
+                 const Tensor<1, dim>& normal,
+                 Vector<double>&       flux)
+   {
+      upwind_flux(ul, ur, p, normal, flux);
+   }
+
+   //---------------------------------------------------------------------------
+   void
+   char_mat(const Vector<double>& /*sol*/,
+            const Point<2>&       /*p*/,
+            const Tensor<1, 2>&   /*ex*/,
+            const Tensor<1, 2>&   /*ey*/,
+            FullMatrix<double>&   Rx,
+            FullMatrix<double>&   Lx,
+            FullMatrix<double>&   Ry,
+            FullMatrix<double>&   Ly)
+   {
+      Rx[0][0] = 1.0;
+      Ry[0][0] = 1.0;
+      Lx[0][0] = 1.0;
+      Ly[0][0] = 1.0;
+   }
+
+   //---------------------------------------------------------------------------
+   void print_info()
+   {
+   }
+
+   //---------------------------------------------------------------------------
+   template <int dim>
+   class Postprocessor : public DataPostprocessor<dim>
+   {
+   public:
+      void
+      evaluate_scalar_field(const DataPostprocessorInputs::Scalar<dim> &input_data,
+                            std::vector<Vector<double>> &computed_quantities) const override
+      {
+         const std::vector<double> &uh = input_data.solution_values;
+         for (unsigned int q = 0; q < uh.size(); ++q)
+         {
+            computed_quantities[q][0] = uh[q];
+         }
+      }
+      std::vector<std::string> get_names() const override
+      {
+         return {"Solution"};
+      }
+      UpdateFlags get_needed_update_flags() const override
+      {
+         return update_values;
+      }
+   };
+
+}
+#endif
---
+++ dg2d/models/linadv/rotate.h
@@ -0,0 +1,41 @@
+// Linear advection on circles
+
+//------------------------------------------------------------------------------
+namespace ProblemData
+{
+   const std::string name = "Advection on circles";
+   const double xmin = -1.0;
+   const double xmax =  1.0;
+   const double ymin = -1.0;
+   const double ymax =  1.0;
+   const double final_time = 2.0 * M_PI;
+   const bool periodic_x = true;
+   const bool periodic_y = true;
+
+   //---------------------------------------------------------------------------
+   // Velocity field
+   //---------------------------------------------------------------------------
+   template <>
+   void velocity(const Point<2>& p, Tensor<1,2>& v)
+   {
+      v[0] = -p[1];
+      v[1] =  p[0];
+   }
+}
+
+//------------------------------------------------------------------------------
+template <int dim>
+struct Problem : ProblemBase<dim>
+{
+   const double alpha = 50.0;
+   const double x0 = 0.5;
+   const double y0 = 0.0;
+
+   void initial_value(const Point<dim>& p,
+                      Vector<double>&   u) const override
+   {
+      const double x = p[0] - x0;
+      const double y = p[1] - y0;
+      u[0] = exp(-alpha * (x * x + y * y));
+   }
+};
