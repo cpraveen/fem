ed4f8fcaa824ca13bf6acae716b900f6d44e1347
Praveen C <cpraveen@gmail.com>
2025-03-01 17:19:12 +0530

    Add local refinement example

--- deal.II/misc/tria_refine/demo.cc
+++ deal.II/misc/tria_refine/demo.cc
@@ -9,7 +9,7 @@
 
 using namespace dealii;
 
-int main()
+void test1()
 {
    Triangulation<2> triangulation;
 
@@ -44,4 +44,52 @@ int main()
                 << triangulation.n_cells()
                 << std::endl;
    }
+}
+
+void test2()
+{
+   Triangulation<2> triangulation;
+
+   {
+      Triangulation<2> tmp_tria;
+      GridGenerator::subdivided_hyper_cube (tmp_tria, 4, 0, 1);
+      GridGenerator::convert_hypercube_to_simplex_mesh(tmp_tria, triangulation);
+   }
+
+   MappingFE<2> mapping(FE_SimplexP<2>(1));
+
+   const Point<2> center(0.5, 0.5);
+   for (const auto &cell : triangulation.active_cell_iterators())
+   {
+      for (const auto v : cell->vertex_indices())
+      {
+         const double distance_from_center =
+              center.distance(cell->vertex(v));
+ 
+         if (distance_from_center < 0.25)
+         {
+            cell->set_refine_flag();
+            break;
+         }
+      }
+   }
+ 
+   triangulation.execute_coarsening_and_refinement();
+
+   {
+      std::ofstream outfile("grid3.svg");
+      GridOut().write_svg(triangulation, outfile);
+      std::cout << "   Number of active cells: "
+                << triangulation.n_active_cells()
+                << std::endl
+                << "   Total number of cells: "
+                << triangulation.n_cells()
+                << std::endl;
+   }
+}
+
+int main()
+{
+   test1();
+   test2();
 }
