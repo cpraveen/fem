b314fa7ea8036858b54b8605fcd89754ba3f6886
Praveen C <cpraveen@gmail.com>
2025-09-23 13:32:23 +0530

    Copy some examples from firedrake website

---
+++ firedrake/burgers.py
@@ -0,0 +1,117 @@
+# Burgers equation
+# ================
+#
+# The Burgers equation is a non-linear equation for the advection and
+# diffusion of momentum. Here we choose to write the Burgers equation in
+# two dimensions to demonstrate the use of vector function spaces:
+#
+# .. math::
+#
+#    \frac{\partial u}{\partial t} + (u\cdot\nabla) u - \nu\nabla^2 u = 0
+#
+#    (n\cdot \nabla) u = 0 \ \textrm{on}\ \Gamma
+#
+# where :math:`\Gamma` is the domain boundary and :math:`\nu` is a
+# constant scalar viscosity. The solution :math:`u` is sought in some
+# suitable vector-valued function space :math:`V`. We take the inner
+# product with an arbitrary test function :math:`v\in V` and integrate
+# the viscosity term by parts:
+#
+# .. math::
+#
+#    \int_\Omega\frac{\partial u}{\partial t}\cdot v +
+#    ((u\cdot\nabla) u)\cdot v + \nu\nabla u\cdot\nabla v \ \mathrm d x = 0.
+#
+# The boundary condition has been used to discard the surface
+# integral. Next, we need to discretise in time. For simplicity and
+# stability we elect to use a backward Euler discretisation:
+#
+# .. math::
+#
+#    \int_\Omega\frac{u^{n+1}-u^n}{dt}\cdot v +
+#    ((u^{n+1}\cdot\nabla) u^{n+1})\cdot v + \nu\nabla u^{n+1}\cdot\nabla v \ \mathrm d x = 0.
+#
+# We can now proceed to set up the problem. We choose a resolution and set up a square mesh::
+
+from firedrake import *
+n = 30
+mesh = UnitSquareMesh(n, n)
+
+# We choose degree 2 continuous Lagrange polynomials. We also need a
+# piecewise linear space for output purposes::
+
+V = VectorFunctionSpace(mesh, "CG", 2)
+V_out = VectorFunctionSpace(mesh, "CG", 1)
+
+# We also need solution functions for the current and the next
+# timestep. Note that, since this is a nonlinear problem, we don't
+# define trial functions::
+
+u_ = Function(V, name="Velocity")
+u = Function(V, name="VelocityNext")
+
+v = TestFunction(V)
+
+# For this problem we need an initial condition::
+
+x = SpatialCoordinate(mesh)
+ic = project(as_vector([sin(pi*x[0]), 0]), V)
+
+# We start with current value of u set to the initial condition, but we
+# also use the initial condition as our starting guess for the next
+# value of u::
+
+u_.assign(ic)
+u.assign(ic)
+
+# :math:`\nu` is set to a (fairly arbitrary) small constant value::
+
+nu = 0.0001
+
+# The timestep is set to produce an advective Courant number of
+# around 1. Since we are employing backward Euler, this is stricter than
+# is required for stability, but ensures good temporal resolution of the
+# system's evolution::
+
+timestep = 1.0/n
+
+# Here we finally get to define the residual of the equation. In the advection
+# term we need to contract the test function :math:`v` with
+# :math:`(u\cdot\nabla)u`, which is the derivative of the velocity in the
+# direction :math:`u`. This directional derivative can be written as
+# ``dot(u,nabla_grad(u))`` since ``nabla_grad(u)[i,j]``:math:`=\partial_i u_j`.
+# Note once again that for a nonlinear problem, there are no trial functions in
+# the formulation. These will be created automatically when the residual
+# is differentiated by the nonlinear solver::
+
+F = (inner((u - u_)/timestep, v)
+     + inner(dot(u,nabla_grad(u)), v) + nu*inner(grad(u), grad(v)))*dx
+
+# We now create an object for output visualisation::
+
+outfile = VTKFile("burgers.pvd")
+
+# Output only supports visualisation of linear fields (either P1, or
+# P1DG).  In this example we project to a linear space by hand.  Another
+# option is to let the :class:`~.vtk_output.VTKFile` object manage the
+# decimation.  It supports both interpolation to linears (the default) or
+# projection (by passing ``project_output=True`` when creating the
+# :class:`~.vtk_output.VTKFile`). Outputting data is carried out using
+# the :meth:`~.vtk_output.VTKFile.write` method of
+# :class:`~.vtk_output.VTKFile` objects::
+
+outfile.write(project(u, V_out, name="Velocity"))
+
+# Finally, we loop over the timesteps solving the equation each time and
+# outputting each result. Firedrake's default solver parameters are used,
+# which amount to applying a full LU decomposition as a preconditioner. ::
+
+t = 0.0
+end = 0.5
+while (t <= end):
+    solve(F == 0, u)
+    u_.assign(u)
+    t += timestep
+    outfile.write(project(u, V_out, name="Velocity"))
+
+# A python script version of this demo can be found :demo:`here <burgers.py>`.
---
+++ firedrake/helmholtz.py
@@ -0,0 +1,142 @@
+# Simple Helmholtz equation
+# =========================
+#
+# Let's start by considering the modified Helmholtz equation on a unit square,
+# :math:`\Omega`, with boundary :math:`\Gamma`:
+#
+# .. math::
+#
+#    -\nabla^2 u + u &= f
+#
+#    \nabla u \cdot \vec{n} &= 0 \quad \textrm{on}\ \Gamma
+#
+# for some known function :math:`f`. The solution to this equation will
+# be some function :math:`u\in V`, for some suitable function space
+# :math:`V`, that satisfies these equations. Note that this is the
+# Helmholtz equation that appears in meteorology, rather than the
+# indefinite Helmholtz equation :math:`\nabla^2 u + u = f` that arises
+# in wave problems.
+#
+# We transform the equation into weak form by multiplying by an arbitrary
+# test function in :math:`V`, integrating over the domain and then
+# integrating by parts. The variational problem so derived reads: find
+# :math:`u \in V` such that:
+#
+# .. math::
+#
+#    \require{cancel}
+#    \int_\Omega \nabla u\cdot\nabla v  + uv\ \mathrm{d}x = \int_\Omega
+#    vf\ \mathrm{d}x + \cancel{\int_\Gamma v \nabla u \cdot \vec{n} \mathrm{d}s}
+#
+# Note that the boundary condition has been enforced weakly by removing
+# the surface term resulting from the integration by parts.
+#
+# We can choose the function :math:`f`, so we take:
+#
+# .. math::
+#
+#    f = (1.0 + 8.0\pi^2)\cos(2\pi x)\cos(2\pi y)
+#
+# which conveniently yields the analytic solution:
+#
+# .. math::
+#
+#    u = \cos(2\pi x)\cos(2\pi y)
+#
+# However we wish to employ this as an example for the finite element
+# method, so lets go ahead and produce a numerical solution.
+#
+# First, we always need a mesh. Let's have a :math:`10\times10` element unit square::
+
+from firedrake import *
+mesh = UnitSquareMesh(10, 10)
+
+# We need to decide on the function space in which we'd like to solve the
+# problem. Let's use piecewise linear functions continuous between
+# elements::
+
+V = FunctionSpace(mesh, "CG", 1)
+
+# We'll also need the test and trial functions corresponding to this
+# function space::
+
+u = TrialFunction(V)
+v = TestFunction(V)
+
+# We declare a function over our function space and give it the
+# value of our right hand side function::
+
+f = Function(V)
+x, y = SpatialCoordinate(mesh)
+f.interpolate((1+8*pi*pi)*cos(x*pi*2)*cos(y*pi*2))
+
+# We can now define the bilinear and linear forms for the left and right
+# hand sides of our equation respectively::
+
+a = (inner(grad(u), grad(v)) + inner(u, v)) * dx
+L = inner(f, v) * dx
+
+# Finally we solve the equation. We redefine `u` to be a function
+# holding the solution::
+
+u = Function(V)
+
+# Since we know that the Helmholtz equation is
+# symmetric, we instruct PETSc to employ the conjugate gradient method
+# and do not worry about preconditioning for the purposes of this demo ::
+
+solve(a == L, u, solver_parameters={'ksp_type': 'cg', 'pc_type': 'none'})
+
+# For more details on how to specify solver parameters, see the section
+# of the manual on :doc:`solving PDEs <../solving-interface>`.
+#
+# Next, we might want to look at the result, so we output our solution
+# to a file::
+
+VTKFile("helmholtz.pvd").write(u)
+
+# This file can be visualised using `paraview <http://www.paraview.org/>`__.
+#
+# We could use the built-in plotting functions of firedrake by calling
+# :func:`tripcolor <firedrake.pyplot.tripcolor>` to make a pseudo-color plot.
+# Before that, matplotlib.pyplot should be installed and imported::
+
+try:
+  import matplotlib.pyplot as plt
+except:
+  warning("Matplotlib not imported")
+
+try:
+  from firedrake.pyplot import tripcolor, tricontour
+  fig, axes = plt.subplots()
+  colors = tripcolor(u, axes=axes)
+  fig.colorbar(colors)
+except Exception as e:
+  warning("Cannot plot figure. Error msg: '%s'" % e)
+
+# The plotting functions in Firedrake mimic those of matplotlib; to produce a
+# contour plot instead of a pseudocolor plot, we can call
+# :func:`tricontour <firedrake.pyplot.tricontour>` instead::
+
+try:
+  fig, axes = plt.subplots()
+  contours = tricontour(u, axes=axes)
+  fig.colorbar(contours)
+  plt.savefig("helmholtz.png")
+except Exception as e:
+  warning("Cannot plot figure. Error msg: '%s'" % e)
+
+# Don't forget to show the image::
+
+try:
+  plt.show()
+except Exception as e:
+  warning("Cannot show figure. Error msg: '%s'" % e)
+
+# Alternatively, since we have an analytic solution, we can check the
+# :math:`L_2` norm of the error in the solution::
+
+f.interpolate(cos(x*pi*2)*cos(y*pi*2))
+print(sqrt(assemble(dot(u - f, u - f) * dx)))
+
+# A python script version of this demo can be found :demo:`here <helmholtz.py>`.
