c5fa52c6f2a3ff3f97131a837631ce3d2e390e32
Praveen C <cpraveen@gmail.com>
2025-11-26 21:51:22 +0530

    Use observer pointer

--- deal.II/misc/smart_pointer/demo.cc
+++ deal.II/misc/smart_pointer/demo.cc
@@ -1,22 +1,25 @@
-#include <deal.II/base/smartpointer.h>
+#include <deal.II/base/observer_pointer.h>
 #include <deal.II/fe/fe_q.h>
 
 using namespace dealii;
 
 int main()
 {
-   SmartPointer<FiniteElement<2>> fe1;   // SmartPointer
-   //FiniteElement<2>* fe1;              // normal pointer
+   FiniteElement<2>* fe1;                   // normal pointer
+   ObserverPointer<FiniteElement<2>> fe2;   // ObserverPointer
    {
       FE_Q<2> fe(2);
       fe1 = &fe;
-      std::cout << "Degree = " << fe1->degree << std::endl;
+      fe2 = &fe;
+      std::cout << "Normal   pointer: Degree = " << fe1->degree << std::endl;
+      std::cout << "Observer pointer: Degree = " << fe2->degree << std::endl;
    }
-   // fe has been destroyed, next line should give an error for SmartPointer
-   // If you dont use SmartPointer, you may not get any error message,
+   // fe has been destroyed, next line should give an error for ObserverPointer
+   // If you dont use ObserverPointer, you may not get any error message,
    // some random thing may happen, result is not guaranteed to be correct.
    // It will be very difficult to catch such errors because code will run
    // but may give wrong answer.
-   std::cout << "Degree = " << fe1->degree << std::endl;
+   std::cout << "Normal   pointer: Degree = " << fe1->degree << std::endl;
+   std::cout << "Observer pointer: Degree = " << fe2->degree << std::endl;
    return 0;
 }
